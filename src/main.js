ba3d2f0d3bf7b243fcea6217fe3f4e82247b4006
Swapnanil1 <135691331+swapnanil1@users.noreply.github.com>
2025-05-28 19:18:22 +0530

    stop flashing svgs | need to find a async way of doing it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import './style.css'
 import miscEvents from './utils/navbar_events'
 import imgCarousel from './utils/image_carousel'
+// stop flashing
+const tuxPng = document.getElementById('tuxPNG')
+tuxPng.addEventListener('load', () => {
+    tuxPng.style.display = 'block'
+})
+const profilePNG = document.getElementById('profilePNG')
+profilePNG.addEventListener('load', () => {
+    profilePNG.style.display = 'block'
+})
 miscEvents()
 imgCarousel()
